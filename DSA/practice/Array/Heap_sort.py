087d23922788795b522b6732df23b274da624feb
pratik0157-hub <pratik0157@gmail.com>
2026-08-10 22:33:11 +0530

    heap sort intro

---
+++ DSA/practice/Array/Heap_sort.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def hepify(arr, n, i):
+    size = len(arr)
+    if size%2 == 0:
+        l=arr[size-1]
+        parent = arr[size//2]
+    else:
+        l = arr[size-2]
+        r = arr[size-1]
+        parent = arr[size//2]
+    if l > parent:
+        l, parent = parent, l
+    if r > parent:
+        r, parent = parent, r
+    
+
