df4cd9fd0d4681234266b1e4641278393fe18ad5
pratik0157-hub <pratik0157@gmail.com>
2026-08-16 22:34:50 +0530

    heap sort

--- DSA/practice/Array/Heap_sort.py
+++ DSA/practice/Array/Heap_sort.py
@@ -1,20 +1,52 @@
 #!/usr/bin/env python3
+# To heapify a subtree rooted with node i
+def heapify(arr, n, i):
 
-def hepify(arr, n, i):
-    size = len(arr)
-    if size%2 == 0:
-        l=arr[size-1]
-        parent = arr[size//2]
-    else:
-        l = arr[size-2]
-        r = arr[size-1]
-        parent = arr[size//2]
-    if l > parent:
-        l, parent = parent, l
-    if r > parent:
-        r, parent = parent, r
+    # Initialize largest as root
+    largest = i
 
+    # left index = 2*i + 1
+    l = 2 * i + 1
 
-    def heap_sort():
-        return None
- 
+    # right index = 2*i + 2
+    r = 2 * i + 2
+
+    # If left child is larger than root
+    if l < n and arr[l] > arr[largest]:
+        largest = l
+
+    # If right child is larger than largest so far
+    if r < n and arr[r] > arr[largest]:
+        largest = r
+
+    # If largest is not root
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+
+        # Recursively heapify the affected sub-tree
+        heapify(arr, n, largest)
+
+# Main function to do heap sort
+def heapSort(arr):
+    n = len(arr)
+
+    # Build heap (rearrange vector)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    # One by one extract an element from heap
+    for i in range(n - 1, 0, -1):
+
+        # Move current root to end
+        arr[0], arr[i] = arr[i], arr[0]
+
+        # Call max heapify on the reduced heap
+        heapify(arr, i, 0)
+
+if __name__ == "__main__":
+    arr = [9, 4, 3, 8, 10, 2, 5]
+
+    heapSort(arr)
+
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
