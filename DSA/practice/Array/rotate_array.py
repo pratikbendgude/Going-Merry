1e2c96d3d1584f999a6df79344e7891d81ab2fd3
Pratik Bendgude <pratik0157@gmail.com>
2026-08-27 23:11:21 +0530

    rotate using temp array

--- DSA/practice/Array/rotate_array.py
+++ DSA/practice/Array/rotate_array.py
@@ -23,3 +23,39 @@ if __name__ == "__main__":
     # Print the rotated array
     for i in range(len(arr)):
         print(arr[i], end=" ")
+
+
+
+# Python Program to right rotate the array by d positions
+# using temporary array
+
+def rotateArr(arr, d):
+    n = len(arr)
+
+    # Handle case when d > n
+    d %= n
+
+    # Storing rotated version of array
+    temp = [0] * n
+
+    # Copy last d elements to the front of temp
+    for i in range(d):
+        temp[i] = arr[n - d + i]
+
+    # Copy the first n - d elements to the back of temp
+    for i in range(n - d):
+        temp[i + d] = arr[i]
+
+    # Copying the elements of temp in arr
+    # to get the final rotated array
+    for i in range(n):
+        arr[i] = temp[i]
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5, 6]
+    d = 2
+
+    rotateArr(arr, d)
+
+    # Print the rotated array
+    print(' '.join(map(str, arr)))
