3e5abb080bcfb0491df7dd9f8b7c0538c8a8b4b5
Pratik Bendgude <pratik0157@gmail.com>
2026-09-03 08:50:47 +0530

    best approach in Eqilibrium index

--- DSA/practice/Array/Equilibrium_index.py
+++ DSA/practice/Array/Equilibrium_index.py
@@ -56,3 +56,27 @@ if __name__ == "__main__":
     arr = [-7, 1, 5, 2, -4, 3, 0]
 
     print(findEquilibrium(arr))
+
+
+"""[Expected Approach] Running Prefix Sum and Suffix Sum - O(n) Time and O(1) Space"""
+
+
+def equilibriumPoint(arr):
+    prefSum = 0
+    total = sum(arr)
+
+    # Iterate pivot over all the elements
+    # of the array and till prefSum != suffSum
+    for pivot in range(len(arr)):
+        suffSum = total - prefSum - arr[pivot]
+        if prefSum == suffSum:
+            return pivot
+        prefSum += arr[pivot]
+
+    return -1
+
+if __name__ == "__main__":
+    arr = [1, 7, 3, 6, 5, 6]
+
+    result = equilibriumPoint(arr)
+    print(result)
