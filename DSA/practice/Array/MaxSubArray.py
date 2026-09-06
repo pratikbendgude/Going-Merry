3576ce75be464203bd529f027ff085cfd90722c1
Pratik Bendgude <pratik0157@gmail.com>
2026-09-06 21:30:18 +0530

    Max Sub Array

---
+++ DSA/practice/Array/MaxSubArray.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""[Naive Approach] By iterating over all subarrays - O(n^2) Time and O(1) Space"""
+
+def maxSubarraySum(arr):
+    res = arr[0]
+  
+    # Outer loop for starting point of subarray
+    for i in range(len(arr)):
+        currSum = 0
+      
+        # Inner loop for ending point of subarray
+        for j in range(i, len(arr)):
+            currSum = currSum + arr[j]
+          
+            # Update res if currSum is greater than res
+            res = max(res, currSum)
+          
+    return res
+
+if __name__ == "__main__":
+    arr = [2, 3, -8, 7, -1, 2, 3]
+    print(maxSubarraySum(arr))
