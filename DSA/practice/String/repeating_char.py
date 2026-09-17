860eb1204a46b05a711b2bd2464d2b5bd169b025
Pratik Bendgude <pratik0157@gmail.com>
2026-09-17 23:44:30 +0530

    Repeating character using frequency

--- DSA/practice/String/repeating_char.py
+++ DSA/practice/String/repeating_char.py
@@ -24,3 +24,34 @@ def firstRepChar(s):
 # Example usage:
 s = "geeksforgeeks"
 print(firstRepChar(s))
+
+
+"""[Expected Approach] Using Frequency Counting - O(n) time and O(1) auxiliary space"""
+
+
+
+def firstRepChar(s):
+    # Create an array to store the count of characters
+    charCount = [0] * 26
+
+    # Iterate through each character in the string
+    for ch in s:
+
+        # Calculate the index in the array for this character
+        index = ord(ch) - ord('a')
+
+        # If the count of the character is not zero,
+        # it means the character is repeated, so we return it
+        if charCount[index] != 0:
+            return ch
+
+        # Increment the count of the character in the array
+        charCount[index] += 1
+
+    # If no character is repeated, return "-1"
+    return "-1"
+
+
+# Example usage:
+s = "geeksforgeeks"
+print(firstRepChar(s))
